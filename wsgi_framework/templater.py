992450751ae02c165de95bd2176ad57f101e3505
Sergey Pestrikov <sergey_pestrikov@mail.ru>
2023-01-15 22:45:01 +0300

    base.html and menu.html templates + templater changed

--- wsgi_framework/templater.py
+++ wsgi_framework/templater.py
@@ -1,19 +1,19 @@
 # Используем шаблонизатор jinja2
-from jinja2 import Template
-from os.path import join
+
+from jinja2 import FileSystemLoader
+from jinja2.environment import Environment
 
 
 def render(template_name, folder='templates', **kwargs):
     '''
-    :param template_name:
-    :param folder:
-    :param kwargs:
+    :param template_name: имя шаблона
+    :param kwargs: параметры для передачи в шаблон
     :return:
     '''
-    file_path = join(folder, template_name)
-    # Открываем шаблон по имени
-    with open(file_path, encoding='UTF-8') as f:
-        # Читаем
-        template = Template(f.read())
-    # Рендеринг шаблона с параметрами
+    # Создаем объект окружения
+    env = Environment()
+    # Указываем папку для поиска шаблонов
+    env.loader = FileSystemLoader(folder)
+    # Находим шаблон
+    template = env.get_template(template_name)
     return template.render(**kwargs)
